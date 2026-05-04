9d3d91e740f287d025c115313a1d4bc917e4f9e3
Subham <de.subham1@gmail.com>
2026-05-04 16:03:52 +0200

    first commit

---
+++ src/test/java/com/banking/nationalBank/NationalBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.nationalBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
